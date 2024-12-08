be9b60e9163df588eb036caf4c5982fcd30af8da
oliver-lister <oliverlister98@gmail.com>
2024-12-08 13:17:50 +1100

    add types for json data in camelcase

---
+++ src/types/types.ts
@@ -0,0 +1,27 @@
+export interface AccountData {
+  accountCategory: string;
+  accountCode: string;
+  accountCurrency: string;
+  accountIdentifier: string;
+  accountStatus: string;
+  accountName: string;
+  accountType: string;
+  accountTypeBank: string;
+  systemAccount: string;
+  valueType: string;
+  totalValue: number;
+}
+
+export interface RootObject {
+  objectCategory: string;
+  connectionId: string;
+  user: string;
+  objectCreationDate: string;
+  data: AccountData[];
+  currency: string;
+  objectOriginType: string;
+  objectOriginCategory: string;
+  objectType: string;
+  objectClass: string;
+  balanceDate: string;
+}
